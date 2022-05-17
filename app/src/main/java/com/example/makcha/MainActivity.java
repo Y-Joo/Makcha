88977d3b6115d333515f1a80f23ecc22d4c25c4c
Y-Joo <wldudwn99@gmail.com>
2022-05-18 01:12:56 +0900

    Added Departure, ArriveInput

--- app/src/main/java/com/example/makcha/MainActivity.java
+++ app/src/main/java/com/example/makcha/MainActivity.java
@@ -17,41 +17,41 @@ import com.google.android.libraries.places.widget.listener.PlaceSelectionListene
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PlaceSelectionListener{
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Places.initialize(getApplicationContext(), getString(R.string.api_key));
 
-
-
         // Initialize the AutocompleteSupportFragment.
-        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
-                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        AutocompleteSupportFragment departureFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.departure);
+        AutocompleteSupportFragment arriveFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.arrive);
 
         // Specify the types of place data to return.
-        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+        assert departureFragment != null;
+        departureFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+        assert arriveFragment != null;
+        arriveFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+
 
         // Set up a PlaceSelectionListener to handle the response.
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(@NonNull Place place) {
-                // TODO: Get info about the selected place.
-                Log.i(TAG, "Place: " + place.getName() + "\n" + place.getId() + "\n" + place.getLatLng());
-            }
+        departureFragment.setOnPlaceSelectedListener(this);
+        arriveFragment.setOnPlaceSelectedListener(this);
+    }
+    @Override
+    public void onPlaceSelected(@NonNull Place place) {
+        // TODO: Get info about the selected place.
+        Log.i(TAG, "Place: " + place.getName() + "\n" + place.getId() + "\n" + place.getLatLng());
+    }
 
 
-            @Override
-            public void onError(@NonNull Status status) {
-                // TODO: Handle the error.
-                Log.i(TAG, "An error occurred: " + status);
-            }
-
-        });
-
-
-
+    @Override
+    public void onError(@NonNull Status status) {
+        // TODO: Handle the error.
+        Log.i(TAG, "An error occurred: " + status);
     }
 
 }

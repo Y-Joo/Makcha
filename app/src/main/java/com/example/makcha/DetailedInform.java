80f27c6cdaf568d88a89491d9ed6d47b56796600
aquamagic9 <happy990105@naver.com>
2022-05-23 02:00:37 +0900

    add detailed direction card

--- app/src/main/java/com/example/makcha/DetailedInform.java
+++ app/src/main/java/com/example/makcha/DetailedInform.java
@@ -2,7 +2,14 @@ package com.example.makcha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class DetailedInform extends AppCompatActivity {
 
@@ -10,5 +17,82 @@ public class DetailedInform extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_inform);
+
+        LinearLayout detailedPage = (LinearLayout)findViewById(R.id.detailedPage);
+
+
+        //middle 중간 레이아웃 => 이미지 경로 나타냄
+        LinearLayout.LayoutParams middleParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0);
+        middleParams.weight = 4;
+        LinearLayout middleBaseLayout = new LinearLayout(this);
+        middleBaseLayout.setOrientation(LinearLayout.VERTICAL);
+        middleBaseLayout.setLayoutParams(middleParams);
+        makeCard(1, this, "쌍용스윗닷홈, 우성. 건영아파트",  "1660번 버스", middleBaseLayout);
+        makeCard(2, this, "경의중앙선 도심역",  "", middleBaseLayout);
+        makeCard(0, this, "도보 이동 00분 (도심역 -> 덕소역)",  "", middleBaseLayout);
+        
+
+    }
+    //type 0: 도보 , 1: 버스 , 2: 지하철
+    public void makeCard(int type, Context parent, String location, String busInform, LinearLayout middleBaseLayout)
+    {
+        //카드 생성
+        LinearLayout.LayoutParams directionCardParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        directionCardParams.setMargins(40,20,40,0);
+        LinearLayout directionCardLayout = new LinearLayout(this);
+        directionCardLayout.setOrientation(LinearLayout.HORIZONTAL);
+        directionCardLayout.setLayoutParams(directionCardParams);
+        //카드안 사진
+        LinearLayout.LayoutParams directionImageParams = new LinearLayout.LayoutParams(0,120);
+        directionImageParams.weight = 1;
+        ImageView directionImage = new ImageView(this);
+        if (type == 0) {
+            directionImage.setBackgroundResource(R.drawable.bus_image_removebg);
+        }
+        else if (type == 1) {
+            directionImage.setBackgroundResource(R.drawable.subway_image);
+        }
+        else if (type == 2) {
+            directionImage.setBackgroundResource(R.drawable.pave_removebg);
+        }
+        directionImage.setLayoutParams(directionImageParams);
+        directionCardLayout.addView(directionImage);
+        //카드안 텍스트정보
+        LinearLayout.LayoutParams textInformParams = new LinearLayout.LayoutParams(0, 120);
+        textInformParams.weight = 4;
+        LinearLayout textInform = new LinearLayout(this);
+        textInform.setOrientation(LinearLayout.VERTICAL);
+        textInform.setPadding(20,20,20,20);
+        textInform.setLayoutParams(textInformParams);
+
+        LinearLayout.LayoutParams locationTextParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 60);
+        locationTextParams.weight = 5;
+        if (type == 1) {
+            TextView locationText = new TextView(this);
+            locationText.setText(busInform);
+            locationText.setGravity(Gravity.CENTER_VERTICAL);
+            locationText.setTextColor(Color.rgb(204,0,0));
+            locationText.setLayoutParams(locationTextParams);
+            textInform.addView(locationText);
+        }
+
+        TextView locationText2 = new TextView(this);
+        locationText2.setText(location);
+        locationText2.setGravity(Gravity.TOP);
+        if (type == 0) {
+            locationText2.setTextColor(Color.rgb(150,150,150));
+        }
+        else if (type == 1) {
+            locationText2.setTextColor(Color.rgb(0,0,150));
+        }
+        else if (type == 2) {
+            locationText2.setTextColor(Color.rgb(0,150,0));
+        }
+        locationText2.setLayoutParams(locationTextParams);
+        textInform.addView(locationText2);
+
+        directionCardLayout.addView(textInform);
+
+        middleBaseLayout.addView(directionCardLayout);
     }
 }

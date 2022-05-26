398b709de9614de01e0900d435641c88e3be216f
aquamagic9 <happy990105@naver.com>
2022-05-26 18:20:44 +0900

    Fixed Detailedinform Page

--- app/src/main/java/com/example/makcha/DetailedInform.java
+++ app/src/main/java/com/example/makcha/DetailedInform.java
@@ -2,17 +2,20 @@ package com.example.makcha;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -22,8 +25,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class DetailedInform extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +40,148 @@ public class DetailedInform extends AppCompatActivity {
         LinearLayout detailedPage = (LinearLayout)findViewById(R.id.detailedPage);
         //detailedPage.addView(makeTextOnBar());
         //detailedPage.addView(makeImageBar());
+        LinearLayout directionCardList = (LinearLayout)findViewById(R.id.directionCardList);
+
+        /////첫번째 레이아웃 !!!!!!!!!!!!여기에 정보 가져와야함..!!!!
+        //cardview
+        //divider
+        View divider = new View(this);
+        LinearLayout.LayoutParams dividerParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 4);
+        dividerParams.setMargins(20,20,20,20);
+        divider.setBackgroundColor(getColor(com.google.android.libraries.places.R.color.quantum_grey));
+        divider.setLayoutParams(dividerParams);
+        directionCardList.addView(divider);
+        Intent intent = getIntent();
+
+
+        JSONObject detailObj = null;
+        try {
+            detailObj = new JSONObject(intent.getStringExtra("route_detail"));
+            //arrival time, departure time, duration - value
+            String arrival_time = detailObj.getJSONObject("arrival_time").getString("text");
+            Date date = new Date();
+            long departure_time = 0;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                departure_time = (detailObj.getJSONObject("departure_time").getLong("value") - Instant.now().getEpochSecond() + 60) / 60;
+            }
+            long duration = (detailObj.getJSONObject("duration").getLong("value") + 30) / 60;
+            Log.i(TAG, "time_to_departure_time : " + departure_time + "분, duration : " + duration + ", arrival_time : " + arrival_time);
+            Log.i(TAG, "=================================");
+            //60분 00시00분 00시00분 도착 레이아웃만들기
+            DisplayMetrics dm = getResources().getDisplayMetrics();
+            int size = Math.round(5 * dm.density);
+            LinearLayout.LayoutParams topParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            LinearLayout topBaseLayout = new LinearLayout(this);
+            topBaseLayout.setGravity(Gravity.CENTER);
+            topBaseLayout.setOrientation(LinearLayout.HORIZONTAL);
+            topParams.setMargins(size, size, size, size);
+            topBaseLayout.setLayoutParams(topParams);
+
+            //int providedTakingTime = 60; //제공된 걸린시간
+            TextView takingTime = new TextView(getApplicationContext());
+            takingTime.setText(duration + "분");
+            takingTime.setTextSize(20);
+            takingTime.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+            LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            textParams.weight = 1;
+            takingTime.setLayoutParams(textParams);
+
+            //int providedDepartureHours = 0; //제공된 출발 시간
+            //int providedDepartureMinutes = 0;
+            //LocalTime providedDepartureTime = LocalTime.newInstance(providedDepartureHours, providedDepartureMinutes);  //도착 시간
+            TextView departureTime = new TextView(getApplicationContext());
+            departureTime.setText(departure_time + "분");
+            departureTime.setTextSize(20);
+            departureTime.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+            LinearLayout.LayoutParams textParams2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            //textParams2.setMargins(100, 0,0, 0);
+            textParams2.weight = 1;
+            departureTime.setLayoutParams(textParams2);
+
+            //int providedArriveHours = 0; //제공된 도착 시간
+            //int providedArriveMinutes = 0;
+            //LocalTime providedArriveTime = LocalTime.newInstance(providedArriveHours, providedArriveMinutes);  //도착 시간
+            TextView arriveTime = new TextView(getApplicationContext());
+            arriveTime.setText(arrival_time);
+            arriveTime.setTextSize(15);
+            arriveTime.setTextAlignment(TextView.TEXT_ALIGNMENT_TEXT_END);
+            LinearLayout.LayoutParams textParams3 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            arriveTime.setPadding(0,0,50,0);
+            textParams3.weight = 1;
+            arriveTime.setLayoutParams(textParams3);
+            topBaseLayout.addView(takingTime);
+            topBaseLayout.addView(departureTime);
+            topBaseLayout.addView(arriveTime);
+            directionCardList.addView(topBaseLayout);
+            JSONArray stepsArr = detailObj.getJSONArray("steps");
+            long sum_step_duration = 0;
+            ArrayList<Long> step_durations = new ArrayList<>();
+            ArrayList<String> vehicles = new ArrayList<>();
+
+            for(int k = 0;k < stepsArr.length(); k++)
+            {
+                JSONObject stepObj = stepsArr.getJSONObject(k);
+                long step_duration = (stepObj.getJSONObject("duration").getLong("value") + 30) / 60;
+                String vehicle;
+                if (stepObj.getString("travel_mode").equals("TRANSIT"))
+                    vehicle = stepObj.getJSONObject("transit_details").getJSONObject("line").getJSONObject("vehicle").getString("type");
+                else
+                    vehicle = "WALKING";
+                step_durations.add(step_duration);
+                vehicles.add(vehicle);
+                sum_step_duration += step_duration;
+                Log.i(TAG, "step_duration : " + step_duration + "분, vehicle : " + vehicle);
+            }
+            FrameLayout routeFrame = new FrameLayout(this);
+            FrameLayout.LayoutParams routeFrameParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+            routeFrame.setLayoutParams(routeFrameParams);
+            LinearLayout barLayout = new LinearLayout(this);
+            LinearLayout.LayoutParams barLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+            barLayout.setGravity(Gravity.CENTER);
+            barLayout.setOrientation(LinearLayout.HORIZONTAL);
+            barLayoutParams.setMargins(40,0,40,0);
+            barLayout.setLayoutParams(barLayoutParams);
+            LinearLayout barTextLayout = new LinearLayout(this);
+            LinearLayout.LayoutParams barTextLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+            barTextLayoutParams.setMargins(40,0,40,0);
+            barTextLayout.setLayoutParams(barTextLayoutParams);
+            for(int k = 0; k < step_durations.size(); k++){
+                Long step_duration = step_durations.get(k);
+                String vehicle = vehicles.get(k);
+
+                //이미지박스로 경로 나타내기. 빨간색: 버스, 파란색: 지하철, 회색:도보
+
+//        int totalTime = 60; //총소요시간 bar비율에 이용할 예정
+//        int time = 10;  // 소요시간
+//        int type = 0; // 나중에 데이터 제공받으면 while문을 통해서 반복해서 경로 만듬
+                int type;
+                if (vehicle.equals("WALKING"))
+                    type = 0;
+                else if (vehicle.equals("BUS"))
+                    type = 1;
+                else
+                    type = 2;
+                makeImageBar(this, type, sum_step_duration, step_duration, barLayout);
+                //bar 위에 소요시간 텍스트로 표시
+                int textTime = 10; // 소요시간
+                // 나중에 데이터를 받으면 while문을 통해서 반복적으로 text객체 생성
+                makeTextOnBar(this, step_duration, sum_step_duration, barTextLayout);
+            }
+            routeFrame.addView(barLayout);
+            routeFrame.addView(barTextLayout);
+            directionCardList.addView(routeFrame);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ///////////////////////////////////////////첫번째 레이아웃 추가
+
+
+
+
+
+
+
 
         //middle 중간 레이아웃 => 이미지 경로 나타냄
         LinearLayout.LayoutParams middleParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0);
@@ -40,7 +190,6 @@ public class DetailedInform extends AppCompatActivity {
         middleBaseLayout.setOrientation(LinearLayout.VERTICAL);
         middleBaseLayout.setLayoutParams(middleParams);
 
-        Intent intent = getIntent();
         JSONObject route_detail = null;
         try {
             route_detail = new JSONObject(intent.getStringExtra("route_detail"));
